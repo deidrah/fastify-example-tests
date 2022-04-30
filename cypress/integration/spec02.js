7e0fe5d00d35a65ca9c08d9a86fa75f13fa17522
Deidrah <dominikastasik93@gmail.com>
2022-04-30 08:51:02 +0200

    Shows the fruit returned by server

--- cypress/integration/spec02.js
+++ cypress/integration/spec02.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 
 it('shows the fruit returned by the server', () => {
+  cy.intercept('GET', '/fruit').as('fruit');
+  cy.visit('/');
+  cy.wait('@fruit').its('response.body.fruit').then(cy.log)
+    .then(fruit => {
+      cy.contains('#fruit', fruit);
+    });
   // spy on the network call the application makes
   // tip: use https://on.cypress.io/intercept
   //
