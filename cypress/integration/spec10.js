e705620982eeb3b12444612a4f95048c12326037
Deidrah <dominikastasik93@gmail.com>
2022-04-30 11:41:15 +0200

    Delay the response using a callback and a delayed promise

--- cypress/integration/spec10.js
+++ cypress/integration/spec10.js
@@ -1,13 +1,15 @@
 /// <reference types="cypress" />
 
 it('shows the loading element with Promise.delay', () => {
-  // set up an intercept to spy on the GET /fruit endpoint
-  // but delay the request by 2 seconds using
-  // Cypress.Promise.delay(2000) and the response function
-  // hint: see https://on.cypress.io/intercept and
-  // the response callback function
-  // visit the site
-  // make sure the loading element is visible
-  // from the request spy, get the response body
-  // and confirm the fruit returned by the server is visible
-})
+  cy.intercept('GET', '/fruit', (req) => {
+    return Cypress.Promise.delay(2000)
+  }).as('fruit');
+  cy.visit('/');
+  cy.contains('#fruit', 'loading...').should('be.visible');
+  cy.wait('@fruit')
+    .its('response.body.fruit')
+    .then(cy.log)
+    .then((fruit) => {
+      cy.contains('#fruit', fruit).should('be.visible')
+    });
+});
