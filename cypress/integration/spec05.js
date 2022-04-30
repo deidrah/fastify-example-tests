de1e4d34aacf6a19ba0c2450e0d5797852649863
Deidrah <dominikastasik93@gmail.com>
2022-04-30 09:29:55 +0200

    Shows a different fruit after reloading the page

--- cypress/integration/spec05.js
+++ cypress/integration/spec05.js
@@ -1,13 +1,12 @@
 /// <reference types="cypress" />
 
 it('shows a different fruit after reloading the page', () => {
-  // visit the site using https://on.cypress.io/visit
-  // grab the fruit name from the page
-  // (make sure it is not "loading...")
-  //
-  // reload the page using https://on.cypress.io/reload
-  // grab the fruit name from the page again
-  // confirm the fruit name is different
-  //
-  // tip: use nested https://on.cypress.io/then callbacks
+  cy.visit('/');
+  cy.get('#fruit').should('not.include.text', 'loading')
+    .invoke('text')
+    .then((fruit) => {
+      cy.reload();
+      cy.get('#fruit').should('not.include.text', 'loading')
+        .and('not.have.text', fruit);
+    });
 })
