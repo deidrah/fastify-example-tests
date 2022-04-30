e6b46e0c54b5e0bf408b41c60862334d1b4090d0
Deidrah <dominikastasik93@gmail.com>
2022-04-30 11:36:37 +0200

    : Use a stub just once using the times option

--- cypress/integration/spec09.js
+++ cypress/integration/spec09.js
@@ -1,13 +1,25 @@
 /// <reference types="cypress" />
 
 it('returns different fruits', () => {
-  // stub the /fruit endpoint to return "apple" on the first visit
-  // stub the /fruit endpoint to return "grapes" on the second visit
-  // https://on.cypress.io/intercept with "times: *" option
-  //
-  // visit the site
-  //
-  // confirm it shows "apple"
-  // reload the site
-  // confirm it shows "grapes"
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    { fruit: 'grapes' },
+  );
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    { fruit: 'apple' },
+  );
+
+  cy.visit('/');
+  cy.contains('#fruit', 'apple').should('be.visible');
+  cy.reload();
+  cy.contains('#fruit', 'grapes').should('be.visible');
 })
