8c58d1e38d1d3e72a3abf8c39f8f26cbc6e1916c
Deidrah <dominikastasik93@gmail.com>
2022-04-30 11:30:34 +0200

    Import the JSON fixture directly into the spec

--- cypress/integration/spec08.js
+++ cypress/integration/spec08.js
@@ -1,17 +1,10 @@
 /// <reference types="cypress" />
 
-// import the fixture JSON data from the file "../fixtures/apple.json"
+import { fruit } from '../fixtures/apple.json';
 
 it('imports the fixture from JSON file', () => {
-  // print the imported fruit to the Command Log
-  // https://on.cypress.io/log
-  //
-  // intercept the GET call to /fruit with fixture "apple.json"
-  // https://on.cypress.io/intercept
-  //
-  // visit the site
-  // https://on.cypress.io/visit
-  //
-  // confirm the fruit from the fixture is shown on the page
-  // https://on.cypress.io/contains
+  cy.log(`fruit: **${fruit}**`);
+  cy.intercept('GET', '/fruit', { fixture: 'apple.json' });
+  cy.visit('/');
+  cy.contains('#fruit', fruit);
 })
