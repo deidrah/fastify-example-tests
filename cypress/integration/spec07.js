64a88cbff281e8f827e310dbab118d6170ee9bf9
Deidrah <dominikastasik93@gmail.com>
2022-04-30 11:27:50 +0200

    cy.fixture vs cy.intercept with a fixture

--- cypress/integration/spec07.js
+++ cypress/integration/spec07.js
@@ -1,14 +1,9 @@
 /// <reference types="cypress" />
 
 it('uses the fixture to stub and check the page', () => {
-  // load the data from the fixture file "apple.json"
-  // using the command https://on.cypress.io/fixture
-  // intercept the GET call to /fruit with fixture "apple.json"
-  // https://on.cypress.io/intercept
-  //
-  // visit the site
-  // https://on.cypress.io/visit
-  //
-  // confirm the fruit from the fixture is shown on the page
-  // https://on.cypress.io/contains
+  cy.fixture('apple.json').then(cy.log).then(data => {
+    cy.intercept('GET', '/fruit', {fixture: 'apple.json'});
+    cy.visit('/');
+    cy.contains('#fruit', data.fruit).should('be.visible');
+  })
 })
