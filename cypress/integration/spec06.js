1df0ff18d5ef741bf830d52c754c54ba8bb24691
Deidrah <dominikastasik93@gmail.com>
2022-04-30 09:45:02 +0200

    Uses a fixture to stub the network call

--- cypress/integration/spec06.js
+++ cypress/integration/spec06.js
@@ -1,16 +1,9 @@
 /// <reference types="cypress" />
 
 it('shows the loading element then fruit from a fixture', () => {
-  // intercept the GET call to /fruit with fixture "apple.json"
-  // https://on.cypress.io/intercept
-  //
-  // visit the site
-  // https://on.cypress.io/visit
-  //
-  // confirm the "loading..." text is shown
-  // confirm the "apple" text is shown
-  // https://on.cypress.io/contains
-  //
-  // confirm there is no element with the text "loading..."
-  // https://on.cypress.io/should
+  cy.intercept('GET', '/fruit', { fixture: 'apple.json' });
+  cy.visit('/');
+  cy.contains('#fruit', 'loading...').should('be.visible');
+  cy.contains('#fruit', 'apple').should('be.visible');
+  cy.contains('#fruit', 'loading...').should('not.exist');
 })
