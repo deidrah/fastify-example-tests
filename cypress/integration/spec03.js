d2abc7d12dd92a85cc79f122ef693d4a5855bff1
Deidrah <dominikastasik93@gmail.com>
2022-04-30 09:06:34 +0200

    Stub the server response

--- cypress/integration/spec03.js
+++ cypress/integration/spec03.js
@@ -1,18 +1,8 @@
 /// <reference types="cypress" />
 
 it('shows the fruit returned from the test', () => {
-  // stub the network call the application makes
-  // to the server using "GET /fruit"
-  // return "Kiwi" json object
-  // tip: use https://on.cypress.io/intercept
-  //
-  // visit the page
-  // https://on.cypress.io/visit
-  //
-  // wait for the app to make the network call
-  // to make sure the stub was used
-  // https://on.cypress.io/wait
-  //
-  // confirm the application shows the fruit "Kiwi"
-  // https://on.cypress.io/contains
+  cy.intercept('GET', '/fruit', { fruit: 'Kiwi'}).as('fruit');
+  cy.visit('/');
+  cy.wait('@fruit');
+  cy.contains('#fruit', "Kiwi");
 })
