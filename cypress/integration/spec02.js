11b79facec8508da36d34d49e002b94728a51b2e
Deidrah <dominikastasik93@gmail.com>
2022-04-30 08:51:18 +0200

    remove the comments

--- cypress/integration/spec02.js
+++ cypress/integration/spec02.js
@@ -7,20 +7,4 @@ it('shows the fruit returned by the server', () => {
     .then(fruit => {
       cy.contains('#fruit', fruit);
     });
-  // spy on the network call the application makes
-  // tip: use https://on.cypress.io/intercept
-  //
-  // visit the page
-  // https://on.cypress.io/visit
-  //
-  // wait for the app to make the network call
-  // (there might be a delay)
-  // https://on.cypress.io/wait
-  //
-  // from the network call, get the response body
-  // and the name of the fruit and confirm
-  // the fruit is shown on the page
-  // https://on.cypress.io/its
-  // https://on.cypress.io/then
-  // https://on.cypress.io/contains
 })
