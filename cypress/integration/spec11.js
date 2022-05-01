d408e06a49a76e04f7e639809a13ebb9581697d8
Deidrah <dominikastasik93@gmail.com>
2022-05-01 16:24:52 +0200

    Test how the application makes a network request every minute

--- cypress/integration/spec11.js
+++ cypress/integration/spec11.js
@@ -1,14 +1,24 @@
 /// <reference types="cypress" />
 
 it('makes GET /fruit requests every minute', () => {
-  // spy on the GET /fruit endpoint
-  // use the cy.clock command to freeze the timers in the app
-  // https://on.cypress.io/clock
-  // visit the page
-  // wait for the first request to finish and confirm the fruit
-  // advance the clock by one minute using cy.tick command
-  // https://on.cypress.io/tick
-  // wait for the second request to finish and confirm the fruit
-  // advance the clock by one minute one more time
-  // wait for the network call and confirm the fruit
+  cy.intercept('GET', '/fruit').as('getFruit');
+  cy.clock(); //freezes time, intervals etc
+  cy.visit('/');
+  cy.wait('@getFruit').its('response.body.fruit')
+    .then((fruit) => {
+      cy.contains('#fruit', fruit).should('be.visible');
+  });
+  cy.tick(60_000); //advance the time
+  
+  cy.wait('@getFruit').its('response.body.fruit')
+    .then((fruit) => {
+      cy.contains('#fruit', fruit).should('be.visible');
+  });
+
+  cy.tick(60_000); //advance the time
+
+  cy.wait('@getFruit').its('response.body.fruit')
+    .then((fruit) => {
+      cy.contains('#fruit', fruit).should('be.visible');
+  });
 })
