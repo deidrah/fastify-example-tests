6f3a575d5dfca03d14690589b66421ad94bdd8a1
Deidrah <dominikastasik93@gmail.com>
2022-05-01 17:30:36 +0200

    Get the fruits from the test using the cy.request command

--- cypress/integration/spec16.js
+++ cypress/integration/spec16.js
@@ -3,9 +3,27 @@
 it('requests all fruits', () => {
   // request the fruit from the /fruit endpoint
   // using the https://on.cypress.io/request command
-  // from the response get the body object, then the fruit
-  // using the https://on.cypress.io/its command
-  // store each fruit in a Set object
-  // and keep requesting until we see a fruit already in the set
-  // print the collected list of fruits
+  const fruits = new Set()
+
+  function getTheFruit() {
+    cy.request('GET', '/fruit')
+      // from the response get the body object, then the fruit
+      // using the https://on.cypress.io/its command
+      .its('body.fruit')
+      .then(cy.log)
+      .then((fruit) => {
+        // and keep requesting until we see a fruit already in the set
+        if (fruits.has(fruit)) {
+          // print the collected list of fruits
+          cy.log([...fruits].sort().join(', '))
+        } else {
+          // store each fruit in a Set object
+          fruits.add(fruit)
+          cy.wait(1000).then(getTheFruit)
+        }
+      })
+  }
+  getTheFruit()
 })
+
+What
