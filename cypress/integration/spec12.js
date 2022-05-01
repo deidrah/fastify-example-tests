d53b59cafc069bb79c80921a72a8aa1e72fb66bf
Deidrah <dominikastasik93@gmail.com>
2022-05-01 17:12:44 +0200

    Confirm the application does not call the server until one minute has passed

--- cypress/integration/spec12.js
+++ cypress/integration/spec12.js
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 it('does not call API until 60 seconds passed', () => {
   // spy on the GET /fruit call and pass a cy.spy function
   // as the request handler. The callback function does nothing
@@ -7,13 +5,23 @@ it('does not call API until 60 seconds passed', () => {
   // many times it was called = how many times the network
   // call happened
   // see https://on.cypress.io/spy
+  cy.intercept('GET', '/fruit', cy.spy().as('fruit'))
   // see https://on.cypress.io/stubs-spies-and-clocks
   // freeze the clock
   // visit the page
+  cy.clock()
+  cy.visit('/')
   // get the spy via its alias and confirm it was called once
   // then reset the history of calls
+  cy.get('@fruit')
+    .should('have.been.calledOnce')
+    .invoke('resetHistory')
   // tick 59 seconds
+  cy.tick(59_000)
+  cy.get('@fruit').should('not.have.been.called')
   // confirm the spy was not executed = there were no network calls
   // tick 1 second
+  cy.tick(1_000)
   // confirm the spy was executed = there was a network call
+  cy.get('@fruit').should('have.been.calledOnce')
 })
