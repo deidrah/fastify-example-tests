db5b75598ceba057fbfd3a0a9db9e9ea1045f075
Deidrah <dominikastasik93@gmail.com>
2022-05-01 17:22:28 +0200

    Reloads the page until it sees the word "Bananas"

--- cypress/integration/spec14.js
+++ cypress/integration/spec14.js
@@ -1,11 +1,23 @@
-/// <reference types="cypress" />
-
 it('reloads the page until it shows Bananas', () => {
   // visit the page
+  cy.visit('/')
   // if it shows the fruit "Bananas", stop
   // else
   //   wait for 1 second for clarity
   //   reload the page
   //   check again
-  // Tip: use recursion
+  function checkFruit() {
+    cy.get('#fruit')
+      .should('not.have.text', 'loading...')
+      .invoke('text')
+      .then((fruit) => {
+        if (fruit === 'Bananas') {
+          cy.log('Bananas!')
+        } else {
+          cy.wait(1000)
+          cy.reload().then(checkFruit)
+        }
+      })
+  }
+  checkFruit()
 })
