d96f1a9a0f2d22fc8dd1097c9764f85fb05d405a
Deidrah <dominikastasik93@gmail.com>
2023-04-24 18:23:16 +0200

    Update bonus01.cy.js

--- cypress/e2e/bonus01.cy.js
+++ cypress/e2e/bonus01.cy.js
@@ -10,48 +10,31 @@
 
 // this test should check whatever the server returns
 // by spying on the network call
-it('shows zero or more fruits', () => {
-  // spy on GET /fruits call and give it an alias
-  // https://on.cypress.io/intercept
-  // https://on.cypress.io/as
-  //
-  // visit the page /fruits.html
-  // https://on.cypress.io/visit
-  //
-  // the page loads one or more fruits
-  // BUT
-  // it might get no fruits back from the server
-  // wait for the network call to finish
-  // https://on.cypress.io/wait
-  // from the server response, get the list of fruits
-  // and depending on the number of fruits,
-  // check the page to confirm
-  //
-  // if there are no fruits, confirm that
-  // the page contains the text "No fruits found"
-  // https://on.cypress.io/contains
-  // else
-  //   confirm each fruit returned by the server is shown
-  // TIP: you can also verify that all fruits are unique
-})
-
 it('shows zero fruits', () => {
   // instead of reloading the page and sometimes (very rarely)
   // getting zero fruits returned
   // let's stub the response from the server
   // https://on.cypress.io/intercept
+  cy.intercept('GET', '/fruits', [])
   // then visit the page /fruits.html
   // https://on.cypress.io/visit
+  cy.visit('/fruits.html')
   // confirm there are no fruits
   // https://on.cypress.io/contains
+  cy.contains('#fruits', 'No fruits')
 })
 
 it('shows two fruits', () => {
   // let's stub the response from the server
   // and return two fruits: apples and kiwi
   // https://on.cypress.io/intercept
+  cy.intercept('GET', '/fruits', ['apples', 'kiwi'])
   // then visit the page /fruits.html
   // https://on.cypress.io/visit
+  cy.visit('/fruits.html')
   // confirm there are two fruits
+  cy.get('#fruits li').should('have.length', 2)
   // https://on.cypress.io/contains
+  cy.contains('#fruits', 'apples')
+  cy.contains('#fruits', 'kiwi')
 })
