d04e512e6e08bbb9130b5c8b4e10ef8ebdedd24d
Deidrah <dominikastasik93@gmail.com>
2023-04-24 12:01:47 +0200

    Update spec21.cy.js

--- cypress/e2e/spec21.cy.js
+++ cypress/e2e/spec21.cy.js
@@ -2,10 +2,21 @@
 
 it('modifies the JSON responses', () => {
   // intercept the JSON request to "/fruit"
+  let fruit2x = null
+  cy.intercept('GET', '/fruit', (req) => {
+    req.continue((res) => {
+      fruit2x = res.body.fruit =
+        res.body.fruit + res.body.fruit
+    })
+  }).as('fruit')
   // take the fruit sent by the server and change it,
   // for example by concatenating it with itself
   // store the fruit in a variable "fruit2x"
   // visit the page and confirm the network call was made
+  cy.visit('/')
+  cy.wait('@fruit').then(() => {
+    cy.contains('#fruit', fruit2x)
+  })
   // confirm the page contains the "fruit2x" text
   // tip: you need to use "cy.then" to access the variable
   // after you have waited for the network call to complete
