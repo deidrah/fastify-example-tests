66438bb8b997cd32eaaea659692fc022b6fc16db
Deidrah <dominikastasik93@gmail.com>
2023-04-25 07:47:44 +0200

    Update bonus05.cy.js

--- cypress/e2e/bonus05.cy.js
+++ cypress/e2e/bonus05.cy.js
@@ -15,15 +15,29 @@ it('changes the loaded CSS resource', () => {
   // Parse the response text using any method you like
   // and add to the "#fruit" CSS style a red border line.
   // Give the network intercept an alias.
+  cy.intercept('GET', 'style.css', (req) => {
+    delete req.headers['if-none-match']
+    delete req.headers['if-modified-since']
+
+    req.continue((res) => {
+      res.body += '\n#fruit { border: 1px solid red; }'
+    })
+  }).as('style')
   //
   // visit the page
+  cy.visit('/')
   // Confirm the intercept worked
   // and its response status code was 200
   // and not 304 (Not Modified)
-  //
+  cy.wait('@style')
+    .its('response.statusCode')
+    .should('equal', 200)
   // Confirm the added CSS style was applied
   // to the page element
-  // the element should have CSS border
-  // because the width might be a float
-  // just confirm it is a solid red line
+  cy.get('#fruit')
+    // the element should have CSS border
+    .should('have.css', 'border')
+    // because the width might be a float
+    // just confirm it is a solid red line
+    .and('include', 'solid rgb(255, 0, 0')
 })
